829929bf84c6ef4ffd0bef53b931097b08bdfe12
Alvaro Orué <alvaro_orue@hotmail.com>
2023-08-04 01:12:14 -0500

    Agregar atributos

--- src/Backend/WebApi/WebApi/Models/Estudiante.cs
+++ src/Backend/WebApi/WebApi/Models/Estudiante.cs
@@ -2,5 +2,10 @@
 {
     public class Estudiante
     {
+        public string _idEstudiante { get; set; }
+        public string _semestreAcademico { get; set; }
+        public string _fechaIngreso { get; set; }
+        public string _curricula { get; set; }
+
     }
 }
--- src/Backend/WebApi/WebApi/Models/Persona.cs
+++ src/Backend/WebApi/WebApi/Models/Persona.cs
@@ -2,5 +2,18 @@
 {
     public class Persona
     {
+        public string _idPersona { get; set; }
+        public string _nombresona { get; set; }
+        public string _apellidos { get; set; }
+        public string _genero { get; set; }
+        public string _foto { get; set; }
+        public string _direccionActual { get; set; }
+        public string _nroCelular { get; set; }
+        public string _correo { get; set; }
+        public string _contraseña { get; set; }
+        public string _tipoUsuario { get; set; }
+        public string _descripcion { get; set; }
+        public string _estado { get; set; }
+
     }
 }
